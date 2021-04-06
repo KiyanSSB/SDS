aa0870fadfe1a85b48be3a1ba68de80a6a0d4bbb
Gabriel De Lamo <gabrieldelamodutra@hotmail.es>
2021-04-06 19:04:23 +0200

    Añadido los ficheros básicos para la realización del registro de un usuario, así como los dos ficheros base que ejecutan las funcionalidades del cliente y del servidor

---
+++ client/cliente.go
@@ -0,0 +1,5 @@
+package main
+
+/*
+	En este archivo se encuentra todo lo relacionado con la ejecución del código del cliente en relación con el servidor
+*/
---
+++ server/signup.go
@@ -0,0 +1,5 @@
+package main
+
+/*
+	En este fichero se encuentra todo lo relacionado con el registro en el apartado del servidor
+*/
