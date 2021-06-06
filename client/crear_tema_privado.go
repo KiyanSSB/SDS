c6d90238b74bc7dbad1684673eaed5172c2b0d24
Gabriel De Lamo <gabrieldelamodutra@hotmail.es>
2021-06-06 23:37:06 +0200

    falta leer temas, esto es una locura

--- client/crear_tema_privado.go
+++ client/crear_tema_privado.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"crypto/rsa"
 	"crypto/sha512"
+	"crypto/x509"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -12,6 +15,7 @@ func crear_tema_privado(resp Resp) {
 	fmt.Println("Crear un tema PRIVADO")
 	fmt.Println("-------------")
 
+	//COmprobar que el tema no está repetido
 	if gTemas == nil {
 		gTemas = make(map[string]tema)
 	}
@@ -22,12 +26,18 @@ func crear_tema_privado(resp Resp) {
 	fmt.Print("Descripción: ")
 	t.Descripcion = leerTerminal()
 
+	//Comprobamos que el tema no está repetido
 	_, ok := gTemas[t.Titulo]
 	if ok {
 		fmt.Println("Este tema ya existe")
 	} else {
 		gTemas[t.Titulo] = t
 	}
+
+	reqBodyBytes := new(bytes.Buffer)
+	json.NewEncoder(reqBodyBytes).Encode(t)
+
+	println(reqBodyBytes.Bytes())
 
 	//K del tema aleatoria para poder cifrar los temas
 	haseo := sha512.New()
@@ -39,7 +49,7 @@ func crear_tema_privado(resp Resp) {
 	fmt.Println("Escribe el nombre del usuario con el que lo quieres compartir")
 	nombreUsuario := leerTerminal()
 
-	//Enviamos la petición al servidor para que nos devuelva la clave privada del usuario
+	//Enviamos la petición al servidor para que nos devuelva la clave publica del usuario
 	data := url.Values{}
 	data.Set("cmd", "damepubkey")
 	data.Set("user", nombreUsuario)
@@ -49,16 +59,28 @@ func crear_tema_privado(resp Resp) {
 	respuestatonta := Resp{}
 	byteValue, _ := ioutil.ReadAll(r.Body)
 	json.Unmarshal([]byte(byteValue), &respuestatonta)
-	fmt.Println(respuestatonta.Msg)
+	fmt.Println("Clave pública del usuario elegido: ", respuestatonta.Msg)
 	fmt.Println("Codificando contraseña aleatoria de tema con clave pública del usuario")
+	//->>>
+	temaAenvia := encode64(encrypt(compress(reqBodyBytes.Bytes()), keyTema64))
+	println(temaAenvia)
+
+	println("estás detrás de temaEnvia")
+
+	temaJSON, err := json.Marshal(t)
 
 	//Enviamos la contraseña random encriptada con la clave pública al servidor para que la almacena
 	data2 := url.Values{}
 	data2.Set("cmd", "guardarKey")
 	data2.Set("user", nombreUsuario)
 	data2.Set("tema", t.Titulo)
-
 	data2.Set("encriptado", encode64(encrypt(compress([]byte(respuestatonta.Msg)), keyTema64)))
+	data2.Set("file", encode64(encrypt(compress([]byte(temaJSON)), keyTema64))) //Tema encoded
+
+	//Variable para chckear que funciona
+	ficheroRandom := encode64(encrypt(compress([]byte(temaJSON)), keyTema64))
+
+	println("estás detrás de fichero Random")
 
 	r2, err := client.PostForm("https://localhost:10443", data2)
 	chk(err)
@@ -66,7 +88,33 @@ func crear_tema_privado(resp Resp) {
 	respuestaSiCreado := Resp{}
 	byteValue, _ = ioutil.ReadAll(r2.Body)
 	json.Unmarshal([]byte(byteValue), &respuestaSiCreado)
-	print(respuestaSiCreado.Msg)
+	print("ESta es la clave privada del usuario : ", respuestaSiCreado.Msg)
+
+	//Desencriptar la llave privada del usuario
+	privateKet := encode64(decrypt(decompress(decode64(respuestaSiCreado.Msg)), u.KeyData))
+	var pk rsa.PrivateKey
+
+	println("hola que tal ")
+
+	var privateKeyBytes []byte = x509.MarshalPKCS1PrivateKey(&pk)
+
+	json.Unmarshal(decode64(privateKet), &pk)
+
+	_ = ioutil.WriteFile("datos_"+nombreUsuario+"_"+t.Titulo+".json.enc", decode64(ficheroRandom), 0644)
+
+	fichetiro, err := ioutil.ReadFile("datos_" + nombreUsuario + "_" + t.Titulo + ".json.enc")
+	chk(err)
+	ficheritoEnconded := encode64(fichetiro)
+
+	//Igual hace falta un unmarshal
+
+	temaDesencriptado := decrypt(decode64(ficheritoEnconded), privateKeyBytes)
+
+	var temaFinal tema
+
+	json.Unmarshal(temaDesencriptado, &temaFinal)
+
+	fmt.Println(temaDesencriptado)
 
 	Opciones(resp)
 
--- server/crear_vinculo_privado.go
+++ server/crear_vinculo_privado.go
@@ -10,9 +10,16 @@ import (
 func crear_vinculo_privado(w http.ResponseWriter, req *http.Request) {
 	if req != nil {
 
+		req.ParseForm()
+		w.Header().Set("Content-Type", "text/plain") //Cabecera estandard
+
 		usuario := req.Form.Get("user")
 		tema := req.Form.Get("tema")
 		clave := req.Form.Get("encriptado")
+
+		filezilla := req.Form.Get("file") //Recuperamos el file
+
+		_ = ioutil.WriteFile("datos_"+usuario+"_"+tema+".json.enc", decode64(filezilla), 0644)
 
 		println(usuario)
 		println(tema)
@@ -30,6 +37,16 @@ func crear_vinculo_privado(w http.ResponseWriter, req *http.Request) {
 		defer file.Close()
 		jsonFD, err := json.Marshal(&clave)
 		err = ioutil.WriteFile(usuario+"_"+tema+"_priv.json", jsonFD, 0644)
+
+		nameFicherito := "datos_" + usuario + "_" + tema + ".json.enc"
+
+		lectura, err := ioutil.ReadFile(nameFicherito)
+		chk(err)
+		encode64(lectura)
+
+		clavePrivadaUsuario := gUsers[usuario].Data["prikey"]
+
+		response(w, true, clavePrivadaUsuario) //Devolvemos la clave privada del usuario que va a decriptar el tema
 
 		//println(gUsers[usuario].Data["private"])
 		//println(gUsers[usuario].Data["prikey"])
